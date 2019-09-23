453adcb48b59cddee6433ef9ef9360ff1d35cf96
Clifton Wood <clifton.wood@gmail.com>
2019-09-23 18:18:36 -0400

    - Adds more enums for checking. (Just get out of my unstaged list!)

--- c-helpers/time_t.c
+++ c-helpers/time_t.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <time.h>
+#include <glib.h>
+
+#include "gio/gio.h"
 
 void main(int argc, char **argv) {
 	printf ("sizeof time_t is: %ld\n", sizeof(time_t));
+	printf ("G_IO_ERROR_FAILED = %d\n", G_IO_ERROR_FAILED);
+	printf ("G_IO_ERROR_NONE = %d\n", G_IO_ERROR_NONE);
+	printf ("G_IO_ERROR_WOULD_BLOCK = %d\n", G_IO_ERROR_WOULD_BLOCK);
+	printf ("G_IO_ERROR_BROKEN_PIPE = %d\n", G_IO_ERROR_BROKEN_PIPE);
 }
