57f69ef77956c771a6b5d37080a0df421acb035e
Clifton Wood <clifton.wood@gmail.com>
2019-12-26 11:32:13 -0500

    - Adds .pl6 and .t files to those processed in renaming.

--- scripts/refactor.sh
+++ scripts/refactor.sh
@@ -7,25 +7,25 @@ if [ "$1" == "--clean" ]; then
 fi
 
 # Should also exclude $1, but that's not really required.
-filelist=`find . \
-  \( \
-    -name examples \
+for a in pm6 pl6 t; do
+  filelist=`find . \
+    \( \
+      -name examples \
+      -o \
+      -name .touch \
+      -o \
+      -name scripts \
+    \) \
+    -prune \
+    -type f \
     -o \
-    -name .touch \
-    -o \
-    -name scripts \
-    -o \
-    -name t \
-  \) \
-  -prune \
-  -type f \
-  -o \
-  -type f \
-  -name \*.pm6 \
-`
+    -type f \
+    -name \*.$a
+  `
 
-for a in $filelist; do
-  echo $a
-  mv $a $a.ref-bak
-  sed -e "s/$1/$2/" $a.ref-bak > $a
+  for b in $filelist; do
+    echo $b
+    mv $b $b.ref-bak
+    sed -e "s/$1/$2/" $b.ref-bak > $b
+  done
 done
